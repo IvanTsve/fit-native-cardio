70051887522e28a94e165b331954f41a9c5b95fb
IvanTsve <iv_plamenovv@abv.bg>
2026-03-17 00:15:05 +0200

    add navigations

---
+++ src/navigation/MainTabs.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ProfileScreen from '@/screens/profile/ProfileScreen';
+import HomeScreen from '@/screens/home/HomeScreen';
+import WorkoutsScreen from '@/screens/workouts/WorkoutsScreen';
+import AiTrainerScreen from '@/screens/aiTrainer/AiTrainerScreen';
+import AnalyticsScreen from '@/screens/analytics/AnalyticsScreen';
+
+const Tab = createBottomTabNavigator();
+
+export default function MainTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Workouts" component={WorkoutsScreen} />
+      <Tab.Screen name="AiTrainer" component={AiTrainerScreen} />
+      <Tab.Screen name="Analytics" component={AnalyticsScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
---
+++ src/navigation/RootNavigator.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import AuthNavigator from './AuthNavigator';
+import MainTabs from './MainTabs';
+
+export default function RootNavigator() {
+
+  const user = true;
+
+  return (
+    <NavigationContainer>
+      {user ? <MainTabs /> : <AuthNavigator />}
+    </NavigationContainer>
+  );
+}
