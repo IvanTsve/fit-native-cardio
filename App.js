24a54284db6d23a3043094bfc9e488fbef2b8562
IvanTsve <iv_plamenovv@abv.bg>
2026-03-17 00:14:33 +0200

    add babel

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+import React from 'react';
+import RootNavigator from './src/navigation/RootNavigator';
+import  * as CommonStyles from './src/styles/Common';
+
+global.commonStyles = CommonStyles.commonStyles;
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[global.commonStyles.container, {backgroundColor: '#c3c4c7'}]}>
+      {/* <StatusBar style="auto" />  */}
+      <RootNavigator />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
