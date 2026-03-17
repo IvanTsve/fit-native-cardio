602f86f8a6b6be709b6e2af6d81c24ed912cf215
IvanTsve <iv_plamenovv@abv.bg>
2026-03-17 18:40:39 +0200

    Update ProgressCircle.jsx

--- src/components/Common/ProgressCircle.jsx
+++ src/components/Common/ProgressCircle.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Svg, { Circle, Text as SvgText } from 'react-native-svg';
 
 
-export default function ProgressCircle({ percentage }) {
+export default function ProgressCircle({ percentage, size = 64 }) {
     return (
-        <Svg height="100%" width="100%" viewBox="0 0 100 100" >
+        <Svg height={size} width={size} viewBox="0 0 100 100">
             <Circle cx="50" cy="50" r="40" stroke="#9ca3af" strokeWidth="6" fill="transparent" />
             <Circle cx="50" cy="50" r="40" stroke="#12C660" strokeWidth="6" fill="transparent" strokeDasharray={`${percentage} 100`} />
             <SvgText
