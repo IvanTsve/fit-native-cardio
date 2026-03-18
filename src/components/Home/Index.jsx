5ff0237551f39488ff6cca7abe2158dab5f20e33
IvanTsve <iv_plamenovv@abv.bg>
2026-03-18 19:30:28 +0200

    Update Index.jsx

--- src/components/Home/Index.jsx
+++ src/components/Home/Index.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { View } from 'react-native';
 import Greetings from '@/components/Home/Greetings';
 import Progress from '@/components/Home/Progres';
+import WorkoutStats from '@/components/Home/WorkoutStats';
 
 export default function Home() {
   return (
     <View>
       <Greetings />
-      <Progress />
+      <View style={{ marginHorizontal: 30, gap: 30 }}>
+        <Progress />
+        <WorkoutStats />
+      </View>
     </View>
   );
 }
