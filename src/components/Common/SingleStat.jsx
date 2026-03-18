bd5213c6d0a5b95ef35255885ed9b2fbdfb6803e
IvanTsve <iv_plamenovv@abv.bg>
2026-03-18 19:30:09 +0200

    Create SingleStat.jsx

---
+++ src/components/Common/SingleStat.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function SingleStat({ title, value, icon }) {
+    return (
+        <View style={styles.singleStatContainer}>
+            <Text style={styles.singleStatIcon}>{icon}</Text>
+            <View>
+                <Text style={styles.singleStatValue}>{value}</Text>
+                <Text>{title}</Text>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    singleStatContainer: {
+        backgroundColor: '#fff',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        padding: 25,
+        borderRadius: 15,
+        boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.1)',
+        gap: 15,
+    },
+
+    singleStatIcon: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#000',
+    },
+    singleStatValue: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#000',
+    },
+});
