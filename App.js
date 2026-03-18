3047078e49072c684b970e2535fd574de8be7f7f
IvanTsve <iv_plamenovv@abv.bg>
2026-03-18 19:47:34 +0200

    hide status bar

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-// import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import React from 'react';
 import RootNavigator from './src/navigation/RootNavigator';
@@ -11,7 +11,8 @@ global.commonStyles = CommonStyles.commonStyles;
 export default function App() {
   return (
     <View style={[global.commonStyles.container, {backgroundColor: '#c3c4c7'}]}>
-      {/* <StatusBar style="auto" />  */}
+      {/* <StatusBar style="auto" backgroundColor="#12C660" />  */}
+      <StatusBar hidden /> 
       <RootNavigator />
     </View>
   );
