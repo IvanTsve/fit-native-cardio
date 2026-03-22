70486d951afd0d9e3b72795fd88fe79324388d23
IvanTsve <iv_plamenovv@abv.bg>
2026-03-22 23:06:57 +0200

    moved the workouts into separate file

--- src/components/Home/RecentWorkouts.jsx
+++ src/components/Home/RecentWorkouts.jsx
@@ -2,56 +2,9 @@ import React from 'react';
 import { View, Text, TouchableOpacity,StyleSheet } from 'react-native';
 import { commonStyles } from '@/styles/Common';
 import SingleWorkout from '@/components/Common/SingleWorkout';
-
+import mockWorkouts from '@/services/workouts';
 export default function RecentWorkouts() {
-    let mockWorkouts = [
-        {
-            id: 1,
-            name: 'Workout 1',
-            duration: '30 minutes',
-            calories: 300,
-            steps: 1000,
-            image: 'https://via.placeholder.com/150',
-            date: '2026-03-18',
-            time: '10:00:00',
-            distance: 1000,
-        },
-        {
-            id: 2,
-            name: 'Workout 2',
-            duration: '45 minutes',
-            calories: 400,
-            steps: 1500,
-            image: 'https://via.placeholder.com/150',
-            date: '2026-03-18',
-            time: '16:00:00',
-            distance: 1500,
-
-        },
-        {
-            id: 3,
-            name: 'Workout 3',
-            duration: '60 minutes',
-            calories: 500,
-            steps: 2000,
-            image: 'https://via.placeholder.com/150',
-            date: '2026-03-18',
-            time: '21:34:00',
-            distance: 2000,
-        },
-        {
-            id: 4,
-            name: 'Workout 4',
-            duration: '75 minutes',
-            calories: 600,
-            steps: 2500,
-            image: 'https://via.placeholder.com/150',
-            date: '2026-03-18',
-            time: '09:07:00',
-            distance: 2500,
-        },
-    ];
-
+    
 
     return (
         <View>
