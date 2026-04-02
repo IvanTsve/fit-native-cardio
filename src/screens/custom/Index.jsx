59de720e4edbefffbffd3144cd8125be90ac31f3
IvanTsve <iv_plamenovv@abv.bg>
2026-04-02 22:15:58 +0300

    Update Index.jsx

--- src/screens/custom/Index.jsx
+++ src/screens/custom/Index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import CustomWorkout from '@/components/Common/CustomWorkout';
 
 export default function Index() {
     return (
-        <View>
+        <View style={styles.container}>
             <CustomWorkout />
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+});
