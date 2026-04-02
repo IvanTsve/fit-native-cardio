eb950be3baeb88ecee4a80616bc5dc39a0920ebe
IvanTsve <iv_plamenovv@abv.bg>
2026-04-03 00:02:55 +0300

    supabase implementation

--- App.js
+++ App.js
@@ -4,11 +4,26 @@ import React from 'react';
 import RootNavigator from './src/navigation/RootNavigator';
 import  * as CommonStyles from './src/styles/Common';
 
+import { useEffect, useState } from 'react'
+import { StyleSheet, View, FlatList, Text } from 'react-native'
+import { supabase } from './lib/supabase'
+
+
 global.commonStyles = CommonStyles.commonStyles;
 
 
 
 export default function App() {
+
+  const [instruments, setInstruments] = useState([])
+  useEffect(() => {
+    getInstruments()
+  }, [])
+  async function getInstruments() {
+    const { data } = await supabase.from('instruments').select()
+    setInstruments(data)
+  }
+  
   return (
     <View style={[global.commonStyles.container, {backgroundColor: '#c3c4c7'}]}>
       {/* <StatusBar style="auto" backgroundColor="#12C660" />  */}
