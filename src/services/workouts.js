607e4d492dac422fa51e19959fe192323ba94331
IvanTsve <iv_plamenovv@abv.bg>
2026-04-03 00:55:36 +0300

    workout services added

--- src/services/workouts.js
+++ src/services/workouts.js
@@ -9,4 +9,41 @@ async function getWorkouts() {
     }
     return data
 }
-export { getWorkouts };
+
+async function getWorkoutById(id) {
+    const { data, error } = await supabase.from('workouts').select().eq('id', id);
+    if (error) {
+        console.error(error)
+        return []
+    }
+    return data
+}
+
+async function createWorkout(workout) {
+    const { data, error } = await supabase.from('workouts').insert(workout);
+    if (error) {
+        console.error(error)
+        return []
+    }
+    return data
+}
+
+async function updateWorkout(id, workout) {
+    const { data, error } = await supabase.from('workouts').update(workout).eq('id', id);
+    if (error) {
+        console.error(error)
+        return []
+    }
+    return data
+}
+
+async function deleteWorkout(id) {
+    const { data, error } = await supabase.from('workouts').delete().eq('id', id);
+    if (error) {
+        console.error(error)
+        return []
+    }
+    return data
+}
+
+export { getWorkouts, getWorkoutById, createWorkout, updateWorkout, deleteWorkout };
